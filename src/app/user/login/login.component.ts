12c4e5f20fc1d39d21eabe130f6e2a048a8d70ba
fumzy <you@example.comnanya.loveth@gmail.com>
2022-05-12 12:03:26 -0400

    Created Login Component.

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  credentials = {
+    email: '',
+    password: ''
+  }
+  showAlert = false
+alertMsg= "Please wait! Your account is being created."
+alertColor = 'blue'
+login()
+{
+  console.log(this.credentials)
+  this.showAlert = true
+  this.alertMsg= "Logging you in...Please wait!"
+  this.alertColor = 'blue'
+}
   constructor() { }
 
   ngOnInit(): void {
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -11,11 +11,31 @@ export class RegisterComponent  {
     Validators.required,
     Validators.minLength(3)
   ])
-  email= new FormControl('')
-  age= new FormControl('')
-  password = new FormControl('')
-  confirm_password = new FormControl('')
-  phonenumber = new FormControl('')
+  email= new FormControl('', [
+    Validators.required,
+    Validators.email
+  ])
+  age= new FormControl('', [
+    Validators.required,
+    Validators.min(18),
+    Validators.max(120)
+  ])
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ])
+  confirm_password = new FormControl('', [
+    Validators.required
+  ])
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.minLength(13),
+    Validators.maxLength(13)
+  ])
+
+showAlert = false
+alertMsg= "Please wait! Your account is being created."
+alertColor = 'blue'
 
 registerForm = new FormGroup({
 name: this.name,
@@ -23,6 +43,12 @@ email: this.email,
 age: this.age,
 password: this.password,
 confirm_password: this.confirm_password,
-phonenumber: this.phonenumber
+phoneNumber: this.phoneNumber
 })
+
+register(){
+  this.showAlert = true
+  this.alertMsg= "Please wait! Your account is being created."
+  this.alertColor = 'blue'
+}
 }
